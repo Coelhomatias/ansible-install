e3d5ba15f3c8efc56537b4c7fc1b4c4bf06d74e3
Coelhomatias <lenadrohenrique.lf@gmail.com>
2024-09-13 17:39:20 +0100

    fixed a bug related to nvm

--- install.sh
+++ install.sh
@@ -29,3 +29,8 @@ fi
 
 echo "Running the ansible playbook. Please enter your sudo password when prompted."
 ansible-playbook playbook.yml -K
+
+echo "Installing node LTS..."
+nvm install --lts --latest-npm
+
+echo "Done!"
